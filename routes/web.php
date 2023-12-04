58b8693d4810a91365ca75b9fc94cd2e4dbba6fe
Mirko Spino <mirkospino@gmail.com>
2023-12-04 19:05:05 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\App;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
+
+Route::controller(App::class)->group(function () {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/logout', 'logout')->name('logout');
+});
+Route::resources([
+    'users' => UserController::class,
+]);
