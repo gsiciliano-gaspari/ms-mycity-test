37d85e7e57d4269ba569869ff315b88a8867646d
Mirko Spino <mirkospino@gmail.com>
2023-12-04 13:06:02 -0300

    global vars in Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void
     {
-        //
     }
-
-    /**
-     * Bootstrap any application services.
-     */
+    // Variabili per tutte le pagine
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive();
+        view()->composer('app', function ($view) {
+            $websiteTitle = 'Mirko Spino MyCity Test';
+            $view->with('websiteTitle', $websiteTitle);
+        });
+        view()->composer('*', function ($view) {
+            $bsClassFormLabels = 'col-md-4 col-form-label text-md-end text-start';
+            $bsClassButtons = 'btn btn-primary btn-sm my-2';
+            $view->with('bsClassFormLabels', $bsClassFormLabels);
+            $view->with('bsClassButtons', $bsClassButtons);
+        });
     }
 }
